19679c5b8ba8bd11e6335b65efc974f0b1c67ce4
Marcus <marcusaclayton@gmail.com>
2016-11-10 23:11:17 -0800

    Updates powershell script

--- ps.ps1
+++ ps.ps1
@@ -8,12 +8,12 @@
 }
 #>
 
-$uri = "https://prod-10.westus.logic.azure.com:443/workflows/69dae2c72dc9473a91b1a9fa41e02367/triggers/manual/run?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=rOkHG-V-TNTiPkUtdXkIWJjm5W4_BCL7n34BVPQT2cg"
+$uri = "<URI_HERE>"
 $body = ConvertTo-Json @{
         Name = 'Marcus Clayton'
         Email = "marcus@cco.com"
-        Subject = "Issue 2 from PS"
-        Message = "This is the second filed issue from a PS JSON post."
+        Subject = "Git Issue 1"
+        Message = "First Git issue from PS."
 }
 
 Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json' -Verbose
